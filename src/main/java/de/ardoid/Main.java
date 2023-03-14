4d1090354f63e85eb9cf2c3b9b72faf32bb5e4d5
Karl Sowada <karl@sowada.eu>
2023-03-14 17:50:56 +0100

    use de.ardoid.files XPath to gather data from a config-gui.xml and put it to console

--- src/main/java/de/ardoid/Main.java
+++ src/main/java/de/ardoid/Main.java
@@ -1,7 +1,23 @@
 package de.ardoid;
 
+import de.ardoid.files.ParseXPath;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 public class Main {
+
+    final public static String CFG_FILE = "res/config-gui.xml";
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("webtool-gui started...");
+        System.out.println("----------------------");
+
+        // read list of prj in config file
+        ParseXPath cfgDoc = new ParseXPath(CFG_FILE, false);
+        NodeList siteNamesNl = cfgDoc.getByXPath("//site/@name");
+        for (int siteNamesIx = 0; siteNamesIx < siteNamesNl.getLength(); siteNamesIx++) {
+            Node siteNameNode = siteNamesNl.item(siteNamesIx);
+            System.out.println(siteNameNode.getTextContent());
+        }
     }
 }
